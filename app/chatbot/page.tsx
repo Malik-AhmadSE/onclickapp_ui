59b3201d861cd27d735c9dbca777824385ed21ad
Malik-AhmadSE <ahmadali009ali008@gmail.com>
2026-02-02 15:40:50 +0500

    dashboard layout of chatbot

--- app/chatbot/page.tsx
+++ app/chatbot/page.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { AppSidebar } from "@/components/app-sidebar"
+import { SiteHeader } from "@/components/site-header"
+import {
+    SidebarInset,
+    SidebarProvider,
+} from "@/components/ui/sidebar"
 
-function page() {
+
+
+export default function Page() {
     return (
-        <div>page</div>
+        <SidebarProvider
+            style={
+                {
+                    "--sidebar-width": "calc(var(--spacing) * 72)",
+                    "--header-height": "calc(var(--spacing) * 12)",
+                } as React.CSSProperties
+            }
+        >
+            <AppSidebar variant="inset" />
+            <SidebarInset>
+                <SiteHeader />
+                <div className="flex bg-foreground rounded-bl-[8px] rounded-br-[8px] flex-1 flex-col">
+                    <div className="@container/main flex flex-1 flex-col gap-2">
+                        <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+
+
+
+                        </div>
+                    </div>
+                </div>
+            </SidebarInset>
+        </SidebarProvider>
     )
 }
-
-export default page
---
+++ components/site-header.tsx
@@ -0,0 +1,16 @@
+
+
+export function SiteHeader() {
+  return (
+    <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b bg-foreground rounded-tl-lg rounded-tr-lg transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
+      <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
+
+
+        <h1 className="text-base text-black font-medium">AI Chatbot</h1>
+        <div className="ml-auto flex items-center gap-2">
+
+        </div>
+      </div>
+    </header>
+  )
+}
